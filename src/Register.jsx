8c9c883e47e65d4cd4729ac2b67ba54c34755701
khalidkao <khalidkao@example.com>
2023-11-29 21:11:09 +0100

    first commit

---
+++ src/Register.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const [groupeSanguin, setGroupeSanguin] = useState('');
+    const [age, setAge] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="age">age</label>
+            <input value={age} onChange={(e) => setAge(e.target.value)}type="number" placeholder="Age" id="age" name="age" />
+            <label htmlFor="groupeSanguin">groupeSanguin</label>
+            <select values={groupeSanguin} onChange={(e) => setGroupeSanguin(e.target.value)}id="groupeSanguin" name="groupeSanguin" >
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+            </select>
+            <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
